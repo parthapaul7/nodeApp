d57253058e7416c1e0aa8b8ee0263190658c045c
Partha <pparthapaul11@gmail.com>
2023-03-25 15:48:44 +0530

    bug fix image

--- controller/shop.js
+++ controller/shop.js
@@ -11,24 +11,7 @@ exports.getShop = async(req, res, next) => {
 
 exports.postShop = async(req, res, next) => {
     try {
-        const reqBody = {
-            name: req.body.name,
-            password: req.body.password,
-            email: req.body.email,
-            number_: req.body.number_,
-            address_: req.body.address_,
-            total_product: req.body.total_product,
-            total_product_sold: req.body.total_product_sold,
-            joined_date: req.body.joined_date,
-            logged_in: req.body.logged_in,
-            all_product_ids: req.body.all_product_ids,
-            orders_: req.body.orders_,
-            open_time: req.body.open_time,
-            close_time: req.body.close_time,
-            upi_id: req.body.upi_id
-        }
-
-        const shop = await ShopDetails.create(reqBody);
+        const shop = await ShopDetails.create(req.body);
         return res.status(200).json({
             status: "success",
             data: shop
